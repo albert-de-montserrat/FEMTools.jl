d1c9cb856921396eaa0a39aca5bb8e3602642412
albert-de-montserrat <albert.de.montserrat@gmail.com>
2025-11-03 16:55:39 +0100

    dirichlet assembler

--- src/BC.jl
+++ src/BC.jl
@@ -24,3 +24,40 @@ function generate_bc_array(bc_dof, bc_val::Pair{Symbol, T}) where {T<:Number}
     return  fill(value, length(dofs))
 end
 
+set_boundary_condition!(bcs::DirichletBoundaryCondition, args::AbstractArray) = set_boundary_condition!(bcs, (args,))
+
+@generated function set_boundary_condition!(bcs::DirichletBoundaryCondition, args::NTuple{N, AbstractArray}) where N
+    quote 
+        for i in eachindex(bcs.dofs)
+            dofᵢ          = bcs.dofs[i]
+            valᵢ          = bcs.values[i]
+
+            for j in axes(bcs.dofs, 2)
+                # set diagonal entries to 0
+                Base.@nexprs $N I -> begin
+                    @inline
+                    arg = args[I]
+                    if arg isa AbstractMatrix
+                        arg[dofᵢ, j] = 0.0
+                    end
+                end
+            end
+
+            Base.@nexprs $N I -> begin
+                @inline
+                arg = args[I]
+                # set diagonal entries to 1
+                if arg isa AbstractMatrix
+                    arg[dofᵢ, dofᵢ] = 1.0
+                
+                # set entries of the vector to the prescribed value
+                elseif arg isa AbstractVector
+                    arg[dofᵢ]       = valᵢ
+
+                else
+                    error("Unsupported type in args")
+                end
+            end
+        end
+    end
+end
