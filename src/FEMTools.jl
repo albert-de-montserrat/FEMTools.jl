f4580ad32e4dcdbc760f41205b7812ad131f12ae
albert-de-montserrat <albert.de.montserrat@gmail.com>
2025-11-03 16:56:16 +0100

    update entry functions

--- src/FEMTools.jl
+++ src/FEMTools.jl
@@ -1,6 +1,7 @@
 module FEMTools
 
 using ForwardDiff, StaticArrays, SparseArrays, LinearAlgebra, Triangulate
+using Atomix
 
 import Base.getindex, Base.length
 
@@ -19,13 +20,14 @@ include("DoFs.jl")
 export get_dofs
 
 include("BC.jl")
-export DirichletBoundaryCondition
+export DirichletBoundaryCondition, set_boundary_condition!
 
 include("sparsity.jl")
 export preallocate_sparse_matrix
 
 include("assembly.jl")
 export assemble_system!, assemble_sparse_matrix!, assemble_sparse_vector!
+export assemble_system_atomics!
 
 include("coloring.jl")
 export color_mesh
