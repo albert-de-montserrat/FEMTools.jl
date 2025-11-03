4ef80806e01e39bb807fe86734037176b56f413e
albert-de-montserrat <albert.de.montserrat@gmail.com>
2025-11-03 16:56:23 +0100

    update script

--- toying.jl
+++ toying.jl
@@ -1,5 +1,6 @@
 using FEMTools, Triangulate
 using LinearAlgebra, SparseArrays, TimerOutputs
+using GLMakie
 
 function example_domain_qcdt_area(; minangle = 20, maxarea = 0.05)
     triin                   = Triangulate.TriangulateIO()
@@ -12,144 +13,101 @@ function example_domain_qcdt_area(; minangle = 20, maxarea = 0.05)
     return triout
 end;
 
-tri = example_domain_qcdt_area(; maxarea = 1e-3)
+function main(tri)
 
-x            = tri.pointlist[1, :]
-y            = tri.pointlist[2, :]
-element2node = tri.trianglelist
+    x            = tri.pointlist[1, :]
+    y            = tri.pointlist[2, :]
+    element2node = tri.trianglelist
 
-element = T1Element()
-grid = Grid(x, y, element2node, element)
+    element = T1Element()
+    grid = Grid(x, y, element2node, element)
 
-# # grid limits
-# x1 = 0e0
-# x2 = 10e0
-# number of elements
-nel = size(grid)
-# chose element type
-# element = LinearElement1D()
-element = T1Element()
-# create the grid
-nnodes = length(grid)  # number of nodes
+    # # grid limits
+    # x1 = 0e0
+    # x2 = 10e0
+    # number of elements
+    nel = size(grid)
+    # chose element type
+    # element = LinearElement1D()
+    element = T1Element()
+    # create the grid
+    nnodes = length(grid)  # number of nodes
 
-# physical properties
-κ    = 1
-H    = 0
-ttot = 5
-nt   = 100
-dt   = ttot / nt
+    # physical properties
+    κ    = 1
+    H    = 0
+    ttot = 5
+    nt   = 100
+    dt   = ttot / nt / 10
 
-# initial conditions
-Tmax = 2
-σ    = 1e-1
-T    = Tmax * exp.(-((grid.x.-0.5).^2 .+ (grid.y.-0.5).^2) ./ 2σ^2)
+    # initial conditions
+    Tmax = 2
+    σ    = 1e-1
+    T    = Tmax * exp.(-((grid.x.-0.5).^2 .+ (grid.y.-0.5).^2) ./ 2σ^2)
+    T0   = copy(T)
+    # boundary conditions
+    c1 = grid.y => ==(0) => :bot
+    c2 = grid.y => ==(1) => :top
+    c3 = grid.x => ==(0) => :left
+    c4 = grid.x => ==(1) => :right
 
-f = mesh(
-    [grid.x grid.y], 
-    grid.element2node', 
-    colormap = Reverse(:romaO),
-    color = T, 
-    shading = false
-)
+    bc_dof = get_dofs(c1, c2, c3, c4)
+    bc_val = :bot => 0e0, :top => 0e0, :left => 0e0, :right => 0e0
 
-# boundary conditions
-c1 = grid.y => ==(0) => :bot
-c2 = grid.y => ==(1) => :top
-c3 = grid.x => ==(0) => :left
-c4 = grid.x => ==(1) => :right
+    bcs    = DirichletBoundaryCondition(bc_dof, bc_val...)
 
-bc_dof = get_dofs(c1, c2, c3, c4)
-bc_val = :bot => 0e0, :top => 0e0, :left => 0e0, :right => 0e0
+    # Preallocate sparse matrix structure
+    dofs_per_element = 1
+    KG  = preallocate_sparse_matrix(grid.element2node, 3)
+    MG  = preallocate_sparse_matrix(grid.element2node, 3)
+    FG  = zeros(nnodes)
+    rhs = similar(T)
 
-bcs    = DirichletBoundaryCondition(bc_dof, bc_val...)
+    to = TimerOutput()
+    @timeit to "assembly" begin
+        @timeit to "classic" assemble_system!(KG, MG, FG, grid, element, κ, H)
 
-# Preallocate sparse matrix structure
-dofs_per_element = 1
-KG = preallocate_sparse_matrix(grid.element2node, 3)
-MG = preallocate_sparse_matrix(grid.element2node, 3)
-FG = zeros(nnodes)
-
-assemble_system!(KG, MG, FG, grid, element, κ, H)
-
-# Create system matrix (sparse)
-KLG = KG + MG / dt
-
-# Apply boundary conditions to sparse matrix
-@views KLG[bcs.dofs,:]         .= 0.0
-@views KLG[bcs.dofs, bcs.dofs] .= I(length(bcs.dofs))
-
-
-
-@generated function set_boundary_condition!(bcs::DirichletBoundaryCondition, args::NTuple{N, AbstractArray}) where N
-    quote 
-        for i in eachindex(bcs.dofs)
-            dofᵢ          = bcs.dofs[i]
-            valᵢ          = bcs.values[i]
-
-            for j in axes(bcs.dofs, 2)
-                # set diagonal entries to 0
-                Base.@nexprs $N I -> begin
-                    if arg isa AbstractMatrix
-                        arg[dofᵢ, j] = 0.0
-                    end
-                end
-            end
-
-            Base.@nexprs $N I -> begin
-                arg = args[I]
-                # set diagonal entries to 1
-                if arg isa AbstractMatrix
-                    arg[dofᵢ, dofᵢ] = 1.0
-                
-                # set entries of the vector to the prescribed value
-                elseif arg isa AbstractVector
-                    arg[dofᵢ]       = valᵢ
-
-                else
-                    error("Unsupported type in args")
-                end
-            end
-
-
-
-        end
+        KG .= 0e0 
+        # MG .= 0e0 
+        FG .= 0e0
+        @timeit to "atomics" assemble_system_atomics!(KG, MG, FG, grid, element, κ, H)
     end
+    display(to)
+
+    # Create stiffness system matrix
+    KLG = @. KG + MG / dt
+
+    # Apply boundary conditions to sparse matrix
+    set_boundary_condition!(bcs, KLG)
+
+    t = 0
+    for _ in 1:25
+        t += dt
+        # update right-hand side
+        rhs .= (MG * T) ./ dt .+ FG
+        set_boundary_condition!(bcs, rhs)
+        # Solve the linear system
+        T .= KLG \ rhs
+    end
+
+    f = mesh(
+        [grid.x grid.y], 
+        grid.element2node', 
+        colormap = :romaO,
+        color = T, 
+        shading = false
+    )
 end
 
-@b set_boundary_condition!($bcs, $args)
-@code_warntype set_boundary_condition2!(bcs, args)
+tri = example_domain_qcdt_area(; maxarea = 1e-4)
+main(tri)
 
-args = KG, MG, FG
+# using Profile
+# using PProf
 
-
-# t = 0
-
-# f,ax, = lines(T, label="T0")
-
-# b = similar(T)
-
-# @timeit to "Time loop" for _ in 1:nt
-#     t += dt
-#     @timeit to "build RHS" b .= MG / dt * T .+ FG
-
-#     # Apply boundary conditions
-#     bcs.value          .= T_analytical.(grid.x[bcs.dofs], Tmax, t, κ, σ) 
-#     @views b[bcs.dofs] .= bcs.value
-
-#     # Solve the linear system
-#     @timeit to "solve" T .= KLG \ b
-# end
-
-# @show to
-# sol = T_analytical.(grid.x, Tmax, t, κ, σ) 
-# lines!(ax, T, label="T FEM")
-# lines!(ax, sol, label="T analytical")
-# axislegend(ax)
-# display(f)
-
-# function set_boundary_condition!(A::AbstractMatrix, F::AbstractVector, bcs::DirichletBoundaryCondition)
-#     (; dofs, values) = bcs
-# end
+# Profile.clear()
+# @profile for i in 1:10000 assemble_system!(KG, MG, FG, grid, element, κ, H) end
+# pprof()
 
 
-sparsitythermal(grid.element2node, 3)
+@b assemble_system!($(KG, MG, FG, grid, element, κ, H)...)
