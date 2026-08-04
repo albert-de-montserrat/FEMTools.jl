8b5c727ea00ee8998066c785205d8a02771bfebe
albert-de-montserrat <albert.de.montserrat@gmail.com>
2026-08-04 18:00:55 +0200

    Remove trailing blank lines

--- src/mesh/coloring.jl
+++ src/mesh/coloring.jl
@@ -49,7 +49,6 @@ function color_mesh_greedy(mesh)
 
     return colors
 end
-
 """
     color_mesh(mesh)
 
@@ -80,4 +79,3 @@ function generate_element_groups(backend, colors)
     ncolors = maximum(colors)
     return [TDev(findall(==(c), colors)) for c in 1:ncolors]
 end
-
